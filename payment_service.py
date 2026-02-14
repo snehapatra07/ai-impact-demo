f335beff502602fcd9f5c08896d68d3ab3532dc5
snehapatra07 <snehapatra9439@gmail.com>
2026-02-14 11:42:53 +0530

    Update payment processing message for security

--- payment_service.py
+++ payment_service.py
@@ -1,5 +1,5 @@
 def process_payment(amount):
-    print("Processing payment of", amount)
+    print("Processing secure payment of", amount)
 
 def refund_payment(amount):
     print("Refunding", amount)
